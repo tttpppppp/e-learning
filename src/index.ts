aa21c488ba675e945a5afd6a8d40828efe8d1567
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-09-22 16:42:10 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../../src/app.module';
-import { ValidationPipe, BadRequestException } from '@nestjs/common';
-import { GlobalException } from '../../src/core/exception/GlobalException';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from '../app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express, { Request, Response } from 'express';
 
-let cachedServer: any;
-
-async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      exceptionFactory: (errors) => {
-        const result = {};
-        errors.forEach((err) => {
-          if (err.constraints) {
-            result[err.property] = Object.values(err.constraints);
-          }
-        });
-        return new BadRequestException({
-          statusCode: 400,
-          message: 'Validation Error',
-          errors: result,
-        });
-      },
-    }),
-  );
-
-  app.useGlobalFilters(new GlobalException());
-
-  const config = new DocumentBuilder()
-    .setTitle('E-Learning API')
-    .setDescription('API for E-Learning')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
-  await app.init();
-  return server; // ✅ Express instance
-}
+let server: any;
 
 export default async function handler(req: Request, res: Response) {
-  if (!cachedServer) {
-    cachedServer = await bootstrap();
+  if (!server) {
+    const expressApp = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    await app.init();
+    server = expressApp;
   }
-  return cachedServer(req, res); // ✅ Express app callable
+  return server(req, res); // Express app là callable
 }
